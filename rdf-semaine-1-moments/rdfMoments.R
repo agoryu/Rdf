1d0ceabfd31691c10b0815bd8a66fa18a55afb0c
agoryu <vang158@live.fr>
2015-01-15 09:16:37 +0100

    ajout premiere séance

---
+++ rdf-semaine-1-moments/rdfMoments.R
@@ -0,0 +1,71 @@
+# -----------------------------------------------------------------------
+# Extraction d'attributs de forme,
+# Module RdF, reconnaissance de formes
+# Copyleft (C) 2014, Universite Lille 1
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# -----------------------------------------------------------------------
+
+# Chargement d'une image en niveaux de gris
+rdfReadGreyImage <- function (nom) {
+  image <- readImage (nom)
+  if (length (dim (image)) == 2) {
+    image
+  } else {
+    channel (image, 'red')
+  }
+}
+
+# Calcul de la surface d'une forme
+rdfSurface <- function (im) {
+  sum (im)
+}
+
+# Calcul d'un moment geometrique
+rdfMoment <- function (im, p, q) {
+  x <- (1 : (dim (im)[1])) ^ p
+  y <- (1 : (dim (im)[2])) ^ q
+  as.numeric (rbind (x) %*% im %*% cbind (y))
+}
+
+# Calcul d'un moment centre
+rdfMomentCentre <- function (im, p, q) {
+  # Barycentre
+  s <- rdfSurface (im)
+  cx <- rdfMoment (im, 1, 0) / s
+  cy <- rdfMoment (im, 0, 1) / s
+  # Initialiser les vecteurs x et y
+  x <- (1 : (dim (im)[1]) - cx) ^ p
+  y <- (1 : (dim (im)[2]) - cy) ^ q
+  # Calcul du moment centre
+  as.numeric (rbind (x) %*% im %*% cbind (y))
+}
+
+rdfMatriceInertie <-function (im) {
+  #creation de la matrice I
+  i1 <- c(dim(im)[1], 0)
+  i2 <- c(0, dim(im)[2])
+  z.mat = cbind(i1, i2)
+
+  #recuperation valeur propre
+  p <- eigen(z.mat)
+
+  #calcule de p-1
+  pinv <- solve(p$vectors)
+  print(pinv)
+
+  #print(pinv)
+  #r.mat = pinv %*% z.mat %*% i1$vectors
+  #print(r.mat)
+}
