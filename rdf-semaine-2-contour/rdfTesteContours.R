c101934341e50d7bff6180e6ac4476ab7b6fc9d0
agoryu <vang158@live.fr>
2015-01-22 12:03:27 +0100

    ajout tp2

---
+++ rdf-semaine-2-contour/rdfTesteContours.R
@@ -0,0 +1,39 @@
+# -----------------------------------------------------------------------
+# Extraction d'attributs de contours,
+# Module RdF, reconnaissance de formes
+# Copyleft (C) 2014, Universite Lille 1
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# -----------------------------------------------------------------------
+
+# Chargement des fonctions externes
+library ("EBImage")
+source ("rdfContours.R")
+
+# Chargement d'un contour
+nom <- "rdf-carre-80.txt"
+cont <- rdfChargeFichierContour (nom)
+size <- length(cont)
+
+# fourier
+fourier <- rdfTransformeFourier(cont)
+fourierAnn <- rdfAnnuleDescFourier(fourier, 0.6)
+fourierInv <- fft(fourierAnn, TRUE)
+
+#corde
+#corde <- rdfAlgorithmeCorde(cont, 0.5)
+
+# Afficher le contour
+plot (fourierInv, main = nom, type = "o", asp = 1, col = "black",
+      ylim = rev (range (Im (cont))))
