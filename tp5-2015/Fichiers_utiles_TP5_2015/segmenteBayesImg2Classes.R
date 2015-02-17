4c2f424c0e0cce5526bdd52d310ff362668e2722
Gaëtan DEFLANDRE <gaetan.deflandre@gmail.com>
2015-02-17 12:42:39 +0100

    probabilité conditionnelle rectifier

--- tp5-2015/Fichiers_utiles_TP5_2015/segmenteBayesImg2Classes.R
+++ tp5-2015/Fichiers_utiles_TP5_2015/segmenteBayesImg2Classes.R
@@ -19,18 +19,22 @@
 
 # Chargement des fonctions externes
 library ("EBImage")
-#setwd("//Users//macaire//Documents//lagis-pc-serv2//enseignement//enonces//S8 Rdf//TP1")
 source ("rdfSegmentation.R")
-source ("fonction.R")
+#source ("fonction.R")
 
 # Chargement de l'image
 #nom <- "rdf-2-classes-texture-0.png"
 nom <- "2classes_100_100_8bits.png"
 image <- rdfReadGreyImage (nom)
 
+
+
+# Question 1
+# ----------
+
 # Calcul et affichage de son histogramme
 nbins <- 256
-h <- hist (as.vector (image), freq=FALSE, breaks = seq (0, 1, 1 / nbins))
+#h <- hist (as.vector (image), freq=FALSE, breaks = seq (0, 1, 1 / nbins))
 
 # Segmentation par binarisation 0.3
 seuil <- 0.36
@@ -41,66 +45,82 @@ binaire37 <- (image - 0.37) >= 0
 
 # Affichage des deux images
 if (interactive ()) {
-  display (binaire35, "image binaire 0.35")
-  display (binaire36, "image binaire 0.36")
-  display (binaire37, "image binaire 0.37")
+  #display (binaire35, "image binaire 0.35")
+  #display (binaire36, "image binaire 0.36")
+  #display (binaire37, "image binaire 0.37")
 }
 
-stop()
+
+
+# Question 2
+# ----------
 
 # Chargement de l'image omega1
-
 nom <- "2classes_100_100_8bits_omega1.png"
 omega1 <- rdfReadGreyImage (nom)
 
 # Calcul et affichage de son histogramme
-nbins <- 256
-h1 <- hist (as.vector (omega1), freq=FALSE, breaks = seq (0, 1, 1 / nbins))
+#h1 <- hist (as.vector (omega1), freq=FALSE, breaks = seq (0, 1, 1 / nbins))
 
 # Chargement de l'image omega2
-# Calcul et affichage de son histogramme
-# a completer pour h2
-#  Calcul des probas a priori des classes
-nom2 <- "2classes_100_100_8bits_omega2.png"
-omega2 <- rdfReadGreyImage (nom2)
-h2 <- hist (as.vector (omega2), freq=FALSE, breaks = seq (0, 1, 1 / nbins))
+nom <- "2classes_100_100_8bits_omega2.png"
+omega2 <- rdfReadGreyImage (nom)
 
+# Calcul et affichage de son histogramme
+#h2 <- hist (as.vector (omega2), freq=FALSE, breaks = seq (0, 1, 1 / nbins))
+
+#  Calcul des probas a priori des classes
 p_omega1= sum(h1$counts[0:255])/ sum(h$counts[0:255])
 p_omega2= sum(h2$counts[0:255])/ sum(h$counts[0:255])
  
-#  Calcul des probas conditionnelles
-#h$counts[90]
-#h1$counts[90]
-#h$density[90]
-#h1$density[90]
 
-pI = h$counts[80] / sum(h$counts[0:255])
-pw1 = h1$counts[80] / sum(h$counts[0:255])
-pw2 = h2$counts[80] / sum(h$counts[0:255])
-                          
+
+# Question 3
+# ----------
+
+#  Calcul des probas conditionnelles
+#h$counts[80]
+#h1$counts[80]
+#h$density[80]
+#h1$density[80]
+
+X = 91;# gris à 79
+
+p79_I  = h$density[X]
+p79_o1 = h1$density[X] / p_omega1
+p79_o2 = h2$density[X] / p_omega2
+      
+
+
+# Question 4
+# ----------
+
 #  pour le seuil X calcul de l'erreur d'assignation
 somme1 = 0:255
 somme2 = 0:255
 erreur = 0:255
+
+
+stop()
+
 # recherche du minimum
 minimum_erreur = 1;
 seuil_minimum_erreur = 0;
 
-for (X in 1:255) 
-  {
-	# (\sum_{\mathbf{X} \in \hat{\omega_2}} P(\mathbf{X} / \omega_1). P(\omega_1)  
-  	somme1[X+1] = sum( h1$density[(X+1):256]) / sum(h1$density[1:256] )
+for (X in 1:255) {
+    # (\sum_{\mathbf{X} \in \hat{\omega_2}} P(\mathbf{X} / \omega_1). P(\omega_1)  
+    somme1[X+1] = sum( h1$density[(X+1):256]) / sum(h1$density[1:256] )
     somme1[X+1] = somme1[X+1] * p_omega1
-	#\sum_{\mathbf{X} \in \hat{\omega_1}} P(\mathbf{X}  / \omega_2). P(\omega_2)    
- 	  somme2[X+1] = sum( h2$density[(X+1):256]) / sum(h2$density[1:256] )
+    #\sum_{\mathbf{X} \in \hat{\omega_1}} P(\mathbf{X}  / \omega_2). P(\omega_2)    
+    somme2[X+1] = sum( h2$density[(X+1):256]) / sum(h2$density[1:256] )
     somme2[X+1] = somme2[X+1] * p_omega2
 
-  	erreur[X+1] = somme1[X+1] + somme2[X+1]
+    erreur[X+1] = somme1[X+1] + somme2[X+1]
     
 	# seuil corrrespondant à l'erreur minimale
   	if (erreur[X+1] < minimum_erreur ) seuil_minimum_erreur = X
   	if (erreur[X+1] < minimum_erreur ) minimum_erreur = erreur[X+1]
-  }
+}
 print(erreur)
 
 seuil = seuil_minimum_erreur/255 
