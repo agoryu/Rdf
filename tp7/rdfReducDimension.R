f18c42ad3d191035b9fe2a3179438f0cf28a522f
agoryu <vang158@live.fr>
2015-03-05 10:38:54 +0100

    ajout du tp7

---
+++ tp7/rdfReducDimension.R
@@ -0,0 +1,75 @@
+#Elliot Vanegue
+#Gaetant Deflandre
+
+library ("EBImage")
+library("MASS")
+library("lattice")
+
+(load(file='x_app.data'))
+(load(file='classe_app.data'))
+(load(file='x_test.data'))
+(load(file='classe_test.data'))
+
+#//////////// TEST ////////////////
+n_test <- 300
+
+couleur2<-rep('red',n_test)
+couleur2[classe_test==2]='green'
+couleur2[classe_test==3]='blue'
+
+plot(x_test, col = couleur2, main="test")
+
+#//////////// APPRENTISSAGE ////////////////
+n_app <- 300
+
+couleur<-rep('red',n_app)
+couleur[classe_app==2]='green'
+couleur[classe_app==3]='blue'
+
+plot(x_test, col = couleur2, main="donnees de test")
+
+#calcul de la covariance
+covariance <- cov(x_app)
+print("matrice de la covariance")
+print(covariance)
+
+Vp <- eigen(covariance)
+
+#affichage de la pente
+pente <- Vp$vectors[2,1]/Vp$vectors[1,1]
+abline(a = 0, b = pente, col = "blue")
+
+#produit scalaire
+ScalarProduct_test <- x_test
+ScalarProduct_test <- x_test %*% (Vp$vectors[,1] / sqrt(sum(Vp$vectors[,1]*Vp$vectors[,1])))
+
+#projection des points
+x_test_ACP <- x_test
+x_test_ACP[,1] = ScalarProduct_test * Vp$vectors[1,1]
+x_test_ACP[,2] = ScalarProduct_test * Vp$vectors[2,1]
+
+#affichage des points projetes
+points(x_test_ACP[classe_test==1,], col="red")
+points(x_test_ACP[classe_test==2,], col="green")
+points(x_test_ACP[classe_test==3,], col="blue")
+
+#////////////////// ALD ///////////////////////
+x_app_ACP.lda<-lda(ScalarProduct_test, classe_app)
+assigne_app<-predict(x_app_ACP.lda, newdata = ScalarProduct_test)
+# Estimation des taux de bonnes classifications
+table_classification_app <-table(classe_app, assigne_app$class)
+print("matrice de confusion :")
+print(table_classification_app)
+
+# table of correct class vs. classification
+diag(prop.table(table_classification_app, 1))
+# total percent correct
+taux_bonne_classif_app <-sum(diag(prop.table(table_classification_app)))
+print(paste("taux de bonne classification", taux_bonne_classif_app))
+
+# forme : les classe d'assignation fournie par l'ALD
+shape<-rep(1,n_app)
+shape[assigne_app$class==2]=2
+shape[assigne_app$class==3]=3
+# Affichage des projections apprentissage classees
+plot(x_test,col=couleur2,pch=shape,xlab = "X1", ylab = "X2")
